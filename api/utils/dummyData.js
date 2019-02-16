a85f3f581edf2afb3218ec8179caf6a5df6301a1
Chidiebere <chidex5@gmail.com>
2019-02-16 20:06:55 +0100

    feat: Create meal service

---
+++ api/utils/dummyData.js
@@ -0,0 +1,28 @@
+export default {
+  meals: [
+    {
+      id: 1,
+      name: "Fried Rice",
+      size: "Medium",
+      price: "450"
+    },
+    {
+      id: 2,
+      name: "Jollof Rice",
+      size: "Large",
+      price: "550"
+    },
+    {
+      id: 3,
+      name: "Eba & Egusi soup",
+      size: "Small",
+      price: "650"
+    },
+    {
+      id: 4,
+      name: "Beans & Dodo",
+      size: "Medium",
+      price: "400"
+    }
+  ]
+};
