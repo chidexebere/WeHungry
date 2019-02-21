21cfcf654a03e89e0510ae4b0b780afa75ac8173
Chidiebere <chidex5@gmail.com>
2019-02-21 23:45:41 +0100

    feat: update checkout page

--- UI/js/nav.js
+++ UI/js/nav.js
@@ -1,13 +1,14 @@
 //selecting dom elements for manipulation
-const toggleSwitch = document.querySelector("#nav-toggle");
+const toggleNav = document.querySelector("#nav-toggle");
 const navigation = document.querySelector("nav ul");
 
 //toggle side nav
 toggleNav.addEventListener("click", () => {
   navigation.slideToggle();
-});
-
-//Hamburger to X toggle
-toggleNav.addEventListener("click", () => {
   this.classList.toggle("active");
 });
+
+// //Hamburger to X toggle
+// toggleNav.addEventListener("click", () => {
+//   this.classList.toggle("active");
+// });
