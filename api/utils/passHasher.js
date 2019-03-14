cd02f9ef7380a8566be0a3057fcffc09e17d6218
Chidiebere <chidex5@gmail.com>
2019-03-14 21:06:37 +0100

    feat: Update services with JWT

---
+++ api/utils/passHasher.js
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+
+let password = "";
+
+/**
+ * @description - Password Hasher
+ *
+ * @param {Object} password
+ *
+ * @returns {Object} token
+ */
+const hashPassword = pwd => {
+  const salt = bcrypt.genSaltSync(15);
+  password = bcrypt.hashSync(pwd, salt);
+  return password;
+};
+
+export default hashPassword;
