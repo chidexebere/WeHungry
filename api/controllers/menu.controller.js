f0a380e2a03f2bf48cf1839aebbe23cf4d2cae22
Chidiebere <chidex5@gmail.com>
2019-03-15 13:31:05 +0100

    feat: Add user controller and Update other controllers

--- api/controllers/menu.controller.js
+++ api/controllers/menu.controller.js
@@ -1,53 +1,65 @@
 import MenuService from "../services/menu.service";
 
-const MenuController = {
-  createMenu(req, res) {
-    /*
-            Expect json of the format
-            {
-                type: "Regular",
-                meals: [
-                        {
-                            name: "Fried Rice",
-                            size: "Medium",
-                            price: "450"
-                        },
-                        {
-                            name: "Jollof Rice",
-                            size: "Large",
-                            price: "550"
-                        }
-                    ]
-            }
-        */
-    const newMenu = req.body;
-    const createdMenu = MenuService.createMenu(newMenu);
-    return res
-      .json({
-        status: "success",
-        data: createdMenu
-      })
-      .status(201);
-  },
-  fetchTodaysMenu(req, res) {
-    const todaysMenu = MenuService.fetchTodaysMenu();
-    return res
-      .json({
-        status: "success",
-        data: todaysMenu
-      })
-      .status(200);
-  },
-  deleteAMenu(req, res) {
-    const id = req.params.id;
-    const filteredmenus = MenuService.deleteAMenu(id);
-    return res
-      .json({
-        status: "success",
-        data: filteredmenus
-      })
-      .status(204);
+/**
+ * menu controller performs controls  request and res -
+ * setup menu for the day and
+ * fetch menu for the day
+ */
+class MenuController {
+  /**
+   * @description add a meal to today menu
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} apiResponse
+   */
+  static async setUpMenu(req, res) {
+    const { id } = req.body;
+    const { user_id } = req.token;
+
+    if (!id) {
+      res.error(400, "meal id is required");
+      return res;
+    }
+    if (Number.isNaN(Number(id))) {
+      res.error(400, "Invalid ID. ID must be a number");
+      return res;
+    }
+
+    try {
+      const addedMeal = await MenuService.setUpMenu(id, user_id);
+      const [rowsUpdate, [updatedMeal]] = addedMeal;
+
+      if (rowsUpdate > 0) {
+        res.status(201, "Meal successfully added to Menu List", updatedMeal);
+      }
+      return res;
+    } catch (error) {
+      res.error(400, error.message);
+      return res;
+    }
   }
-};
+
+  /**
+   * @description retrieve and return menu for the day
+   * @param {object} req
+   * @param {object} res
+   * @returns {Array} menu object array
+   */
+  static async fetchMenu(req, res) {
+    try {
+      const allMenu = await MenuService.fetchMenu();
+
+      if (allMenu.length === 0) {
+        res.status(200, "Menu list for today is empty");
+      } else {
+        res.status(200, null, allMenu);
+      }
+      return res;
+    } catch (error) {
+      res.error(400, error.message);
+      return res;
+    }
+  }
+}
 
 export default MenuController;
--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,55 +1,143 @@
 import OrderService from "../services/order.service";
 
-const OrderController = {
-  createAnOrder(req, res) {
-    /*
-            Expect json of the format
-           {
-                userId: "1",
-                mealId: "4",
-                quantity: "3",
-                status: "delivered"
-            }
-        */
-    const newOrder = req.body;
-    const createdOrder = OrderService.createAnOrder(newOrder);
-    return res
-      .json({
-        status: "success",
-        data: createdOrder
-      })
-      .status(201);
-  },
+/**
+ * order controller performs controls  request and response -
+ * make an order
+ * modify an order
+ * get all orders
+ */
+class OrderController {
+  /**
+   * @description order a meal
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} apiResponse
+   */
+  static makeAnOrder(req, res) {
+    const { customer_id } = req.token;
+    const {
+      caterer_id,
+      meal_id,
+      orderStatus_id,
+      quantity,
+      delivery_cost,
+      total_cost
+    } = req.body;
 
-  editAnOrder(req, res) {
-    /*
-            Enter json of the format
-            {
-                mealId: "4",
-                quantity: "3",
-                status: "delivered"
-            }
-        */
-    const id = req.params.id;
-    const orderUpdate = req.body;
-    const modifiedOrder = OrderService.editAnOrder(id, orderUpdate);
-    return res
-      .json({
-        status: "success",
-        data: modifiedOrder
-      })
-      .status(200);
-  },
+    if (
+      !req.body.meal_id ||
+      !req.body.orderStatus_id ||
+      !req.body.quantity ||
+      !req.body.delivery_cost ||
+      !req.body.total_cost
+    ) {
+      res.status(
+        400,
+        "All parameters are required(meal_id,type,caterer_id)",
+        null
+      );
+      return res;
+    }
 
-  fetchAllOrders(req, res) {
-    const allOrders = OrderService.fetchAllOrders();
-    return res
-      .json({
-        status: "success",
-        data: allOrders
+    if (Number.isNaN(Number(meal_id))) {
+      res.error(400, "Invalid meal_id. meal_id must be a number");
+      return res.send(res);
+    }
+
+    const orderedMeal = OrderService.makeAnOrder(
+      customer_id,
+      caterer_id,
+      meal_id,
+      orderStatus_id,
+      quantity,
+      delivery_cost,
+      total_cost
+    );
+
+    return orderedMeal
+      .then(order => {
+        if (order == null) {
+          res.error(404, "This meal cannot be found");
+        } else {
+          res.status(200, "Your order has been placed", order);
+        }
+        res.send(res);
       })
-      .status(200);
+      .catch(error => {
+        res.error(400, error.message);
+        return res.send(res);
+      });
   }
-};
+
+  /**
+   * @description update an order record
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} apires
+   */
+  static async editAnOrder(req, res) {
+    const {
+      meal_id,
+      orderStatus_id,
+      quantity,
+      delivery_cost,
+      total_cost
+    } = req.body;
+    const { id } = req.params;
+
+    if (Number.isNaN(Number(id)) || Number.isNaN(Number(meal_id))) {
+      res.error(400, "Invalid ID. ID's must be a number");
+      return res;
+    }
+
+    try {
+      const updateOrder = await OrderService.editAnOrder(
+        id,
+        meal_id,
+        orderStatus_id,
+        quantity,
+        delivery_cost,
+        total_cost
+      );
+
+      if (updateOrder === null || updateOrder === 0) {
+        res.error(
+          400,
+          `Order with id ${id} or Meal with id ${meal_id} cannot be found`
+        );
+      } else {
+        res.status(201, "Order was successfully updated");
+      }
+      return res;
+    } catch (error) {
+      res.error(400, error.message);
+      return res;
+    }
+  }
+
+  /**
+   * @description retrieve and return all orders from our data
+   * @param {object} req
+   * @param {object} res
+   * @returns {Array} order object array
+   */
+  static fetchAllOrders(req, res) {
+    const { caterer_id } = req.token;
+    const allOrders = OrderService.fetchAllOrders(caterer_id);
+    return allOrders
+      .then(meals => {
+        if (meals.length === 0) {
+          res.status(200, "No order found!");
+        } else {
+          res.status(200, "Order was successfully fetched!", meals);
+        }
+        res;
+      })
+      .catch(error => {
+        res.error(400, error.message);
+        return res;
+      });
+  }
+}
 
 export default OrderController;
